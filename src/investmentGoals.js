a6957b24db742eb2960d3938e27b841232ddf3f1
Arthur de Senna <sennaa.dev@gmail.com>
2025-04-12 17:15:19 -0300

    feat: investment form and projections file created

---
+++ src/investmentGoals.js
@@ -0,0 +1,61 @@
+function convertToMontlyReturnRate(yearlyReturnRate) {
+  return yearlyReturnRate ** (1 / 12);
+}
+
+function generateReturnsArray(
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = "monthly",
+  monthlyContribution = 0,
+  returnRate = 0,
+  returnTimeFrame = "monthly"
+) {
+  if (!timeHorizon || !startingAmount) {
+    throw new Error("Investimento Inicial e Prazo devem ser informados.");
+  }
+
+  const finalReturnRate =
+    returnTimeFrame === "monthly"
+      ? 1 + returnRate / 100
+      : convertToMontlyReturnRate(1 + returnRate / 100);
+
+  const finalTimeHorizon =
+    timePeriod === "monthly" ? timeHorizon : timeHorizon * 12;
+
+  const referenceInvestmentObject = {
+    investedAmount: startingAmount,
+    interestReturns: 0,
+    totalInterestingReturns: 0,
+    month: 0,
+    totalAmount: startingAmount,
+  };
+
+  const returnsArray = [referenceInvestmentObject];
+
+  for (
+    let timeReference = 1;
+    timeReference <= finalTimeHorizon;
+    timeReference++
+  ) {
+    const totalAmount =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate +
+      monthlyContribution;
+
+    const interestingReturns =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate;
+
+    const investedAmount = startingAmount + monthlyContribution * timeReference;
+
+    const totalInterestingReturns = totalAmount - investedAmount;
+
+    returnsArray.push({
+      investedAmount,
+      interestingReturns,
+      totalInterestingReturns,
+      month: timeReference,
+      totalAmount,
+    });
+  }
+
+  return returnsArray;
+}
